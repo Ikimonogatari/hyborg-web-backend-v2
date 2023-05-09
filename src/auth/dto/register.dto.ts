b2004a8fb06de857bc29433dc95f5fdd71eeab15
bbattulga <battulga.dev@gmail.com>
2023-05-09 12:59:39 +0800

    add request validation pipe

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterDto {
 
     @ApiProperty()
+    @IsEmail()
     email: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     password: string;
 
     @ApiProperty({
         description: "Should match with password"
     })
+    @IsNotEmpty()
     password2: string;
 }
